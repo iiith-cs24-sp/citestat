cee898065db5632c89aafb96d88b188981c30d3a
Shriram Ravindranathan <s20n@ters.dev>
2024-10-19 08:00:11 +0530

    Fix home styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./Home";
-import Author from "./Author";
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home'
+import Author from './Author'
 
 function App() {
-	return (
-		<Router>
-			<div className="container mx-auto px-4 font-jost">
-				<div class="navbar bg-base-300">
-					<Link class="btn btn-ghost text-3xl font-medium" to="/">
-						Citestat
-					</Link>
-				</div>
-				<div>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/author" element={<Author />} />
-					</Routes>
-				</div>
-			</div>
-		</Router>
-	);
-}
 
-export default App;
+  return (
+    <Router>
+      <div className="container mx-auto px-4 font-jost">
+        <div className="navbar bg-base-300">
+          <Link className="btn btn-ghost text-4xl font-medium" to="/">Citestat</Link>
+        </div>
+        <div> 
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/author" element={<Author />} />       
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App
--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,19 +1,20 @@
-const Home = () => {
-	return (
-		<div className="hero min-h-[calc(100vh-91px)]">
-			<div className="hero-content text-center">
-				<div className="max-w-md">
-					<h1 className="text-6xl font-medium">Citestat</h1>
-					<p className="py-3">See your impact on the world.</p>
-					<input
-						type="text"
-						placeholder="Search for Author"
-						className="input input-bordered w-full"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+const Home = ()=>{
+  return (
+   <div className="hero">
+      <div className="hero-content text-center py-36">
+        <div className="max-w-md">
+          <h1 className="text-8xl font-medium">Citestat</h1>
+          <p className="py-3 text-neutral-500 text-xl">
+             See your impact on the world.
+          </p>
+          <input
+             type="text"
+             placeholder="Search for Author"
+             className="input input-bordered w-full text-2xl px-6 py-6" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
