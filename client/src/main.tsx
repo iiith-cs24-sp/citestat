bd523289beccdf4bdd2bcac8471e6f6e5f7c3ae9
Rishabh Sahu <rishabh.sahu@students.iiit.ac.in>
2024-11-06 18:40:29 +0000

    DocumentationTrial maintsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+
+/**
+ * Root element in React Dom
+ */
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
