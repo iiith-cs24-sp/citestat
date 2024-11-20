8efeea0604c62f59eaf0f643a2356b64724a205b
S P <swastik.pal@students.iiit.ac.in>
2024-11-20 22:30:36 +0530

    Removed useless div

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,24 +29,22 @@ function App() {
 				</Link>
 			</div>
 			<div className="container mx-auto px-4 font-jost">
-				<div>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/search" element={<AuthorSearch />} />
-						<Route path="/doi/:doi" element={<DOI />} />
-						<Route path="/author/:orcid" element={<ORCID />} />
-						<Route path="/author" element={<AuthorPage />} />
-						<Route path="*" element={<Navigate to="/" />} />
-						<Route
-							path="/sample"
-							element={
-								<YearCitationChart
-									data={"10.1002/adfm.201505328"}
-								/>
-							}
-						/>
-					</Routes>
-				</div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/search" element={<AuthorSearch />} />
+					<Route path="/doi/:doi" element={<DOI />} />
+					<Route path="/author/:orcid" element={<ORCID />} />
+					<Route path="/author" element={<AuthorPage />} />
+					<Route path="*" element={<Navigate to="/" />} />
+					<Route
+						path="/sample"
+						element={
+							<YearCitationChart
+								data={"10.1002/adfm.201505328"}
+							/>
+						}
+					/>
+				</Routes>
 			</div>
 		</Router>
 	);
