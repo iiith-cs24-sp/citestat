024f0ee4b28fb9a02cb325e4a589981d90db8e3d
S P <swastik.pal@students.iiit.ac.in>
2024-11-29 18:40:57 +0530

    Storage usage monitoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,3 +40,17 @@ function App() {
 }
 
 export default App;
+
+navigator.storage.estimate().then((estimate: StorageEstimate) => {
+	if (estimate.usage == undefined || estimate.quota == undefined) return;
+	const usage = (estimate.usage * 100) / estimate.quota; // percentage
+	if (usage > 80) {
+		console.warn(
+			`Using ${usage.toFixed(3)}% of storage, consider clearing cache`,
+		);
+	} else {
+		console.info(
+			`Using ${usage.toFixed(3)}% of ${(estimate.quota / 1024 / 1024).toFixed(1) + "MB"} bytes of storage`,
+		);
+	}
+});
